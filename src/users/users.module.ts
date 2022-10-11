efdba7b2a643dc6d00ee68ec3532d60811066d33
paul-andre-leveque <p_leveque@digital-campus.net>
2022-10-11 09:21:45 +0200

    modif

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './services/users.service';
-//import { UsersController } from './users/controllers/users.controller';
+import { UsersController } from './users/controllers/users.controller';
 import { User } from './user.entity';
 
 
